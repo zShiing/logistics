95497b8939fcda86e0576613d105965d668dc8a6
zShiing <zx0692@gmail.com>
2019-05-27 22:25:16 +0800

    1.修复启动bug

--- src/main/java/cn/deslak/DemoApplication.java
+++ src/main/java/cn/deslak/DemoApplication.java
@@ -3,9 +3,8 @@ package cn.deslak;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan("cn.deslak.dao")
 public class DemoApplication {
 
